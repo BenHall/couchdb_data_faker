bc7d1fe98aff06596970ee1c5dc6c52970af325e
BenHall <ben@benhall.me.uk>
2010-04-04 22:03:26 +0100

    Updated the document model to be more 'realistic'

--- src/couchdb_converter.rb
+++ src/couchdb_converter.rb
@@ -7,8 +7,9 @@ class CouchdbConverter
       hash = {"id" => a['id'],
               "name" => a['name'],
               "website" => a['website'],
-              "tags" => ["Popular"],
-              "type" => "artist"
+              "tags" => ["Popular", "rock"],
+              "type" => "artist",
+              "photo" => ["http://cdn.7static.com/static/img/artistimages/00/001/434/0000143451_150.jpg"]
              }
       @result << hash.to_json
     end
@@ -22,10 +23,53 @@ class CouchdbConverter
       hash = {"id" => r['id'],
               "type" => "album",
               "title" => r['title'],
-              "tracks" => ["Track 1", "Track 2", "Track 3", "Track 4", "Track 5"],
+              "tracks" => [
+                      {
+                        "order" => 1,
+                        "title" => "Track 1",
+                        "restricted" => [3],
+                        "length" => "3:20",
+                        "price" => ["base" => 0.79, "1" => 0.50, "2" => 0.99],
+                        "preview_link" => "http://www.google.com",
+                        "media_link" => "http://www.bing.com"
+                      },
+                      {
+                        "order" => 2,
+                        "title" => "Track 2",
+                        "restricted" => [1],
+                        "length" => "3:20",
+                        "price" => ["base" => 0.79, "1" => 0.50, "3" => 0.99],
+                        "preview_link" => "http://www.google.com",
+                        "media_link" => "http://www.bing.com"
+                      },
+                      {
+                        "order" => 3,
+                        "title" => "Track 3",
+                        "length" => "3:20",
+                        "price" => ["base" => 0.79, "1" => 0.50, "2" => 0.79, "3" => 0.99],
+                        "preview_link" => "http://www.google.com",
+                        "media_link" => "http://www.bing.com"
+                      },
+                      {
+                        "order" => 4,
+                        "title" => "Track 4",
+                        "restricted" => [1,2],
+                        "length" => "3:20",
+                        "price" => ["base" => 0.79, "3" => 0.99],
+                        "preview_link" => "http://www.google.com",
+                        "media_link" => "http://www.bing.com"
+                      }
+                    ],
               "artist" => artist_id,
               "release_date" => r['releaseDate'],
-              "tags" => [r['releaseYear']],
+              "restricted" => [3],
+              "format" => ["mp3_320", "aac_320"],
+              "price" => ["base" => 4.99, "1" => 1.99, "2" => 4.99],
+              "full_price" => ["base" => 6.99, "1" => 5.99, "2" => 4.99],
+              "preview_link" => "http://www.google.com",
+              "media_link" => "http://www.bing.com",
+              "artwork" => ["http://cdn.7static.com/static/img/sleeveart/00/007/743/0000774315_350.jpg"],
+              "tags" => [r['releaseYear'], "indie"],
               "label" => r['label'],
               "upc" => r['UPC']
              }
